140e9f0b639b95717832d20aa26890c135ed9b63
kbadal19 <k.badal19@gmail.com>
2023-08-25 10:29:33 +0530

    Forecast working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Search from "./components/search/search";
-import CurrentWeather from "./components/current-weather/current-weather";
+import CurrentWeather from "./components/current-weather/curret-weather"
 import Forecast from "./components/forecast/forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
